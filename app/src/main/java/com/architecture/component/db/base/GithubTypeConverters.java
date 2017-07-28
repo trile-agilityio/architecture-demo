28c577dd88708220e314011e4a01981557ae7014
Trieu Phan <trieu.phanvan@asnet.com.vn>
2017-07-28 09:10:22 +0700

    Implement Github api service

---
+++ app/src/main/java/com/architecture/component/db/base/GithubTypeConverters.java
@@ -0,0 +1,23 @@
+package com.architecture.component.db;
+
+import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.util.StringUtil;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GithubTtpeConverters {
+
+    @TypeConverter
+    public static List<Integer> stringToIntList(String data) {
+        if (data == null) {
+            return Collections.emptyList();
+        }
+        return StringUtil.splitToIntList(data);
+    }
+
+    @TypeConverter
+    public static String intListToString(List<Integer> ints) {
+        return StringUtil.joinIntoString(ints);
+    }
+}
---
+++ app/src/main/java/com/architecture/component/service/api/IGithubApi.java
@@ -0,0 +1,30 @@
+package com.architecture.component.service.api;
+
+import android.arch.lifecycle.LiveData;
+
+import com.architecture.component.db.entity.Repo;
+import com.architecture.component.service.response.ResponseApi;
+import com.architecture.component.service.response.SearchResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface IGithubService {
+
+    @GET("repos/{owner}/{name}")
+    LiveData<ResponseApi<Repo>> getRepo(@Path("owner") String owner, @Path("name") String name);
+
+    @GET("users/{login}/repos")
+    LiveData<ResponseApi<List<Repo>>> getRepos(@Path("login") String login);
+
+    @GET("search/repositories")
+    LiveData<ResponseApi<SearchResponse>> searchRepos(@Query("q") String query);
+
+    @GET("search/repositories")
+    Call<SearchResponse> searchRepos(@Query("q") String query, @Query("page") int page);
+
+}
