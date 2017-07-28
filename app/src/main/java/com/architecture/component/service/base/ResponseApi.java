48ffef8d4d3166944e3b1c7ba7e21ddde4ae8d8d
Trieu Phan <trieu.phanvan@asnet.com.vn>
2017-07-28 09:18:06 +0700

    Create Repo DAO

--- app/src/main/java/com/architecture/component/service/base/ResponseApi.java
+++ app/src/main/java/com/architecture/component/service/base/ResponseApi.java
@@ -1,4 +1,4 @@
-package com.architecture.component.service.response;
+package com.architecture.component.service.base;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
